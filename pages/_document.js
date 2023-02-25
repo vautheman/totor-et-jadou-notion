aef2855de25c524a305c934619b52ec8e569e077
vautheman <authirard.victor@gmail.com>
2023-02-25 17:31:05 +0100

    update logo pwa

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/vercel.svg"></link>
-        <meta name="theme-color" content="#042940" />
+        <meta name="theme-color" content="#161E2F" />
       </Head>
       <body className='scrollbar-custom overflow-x-hidden'>
         <Main />
